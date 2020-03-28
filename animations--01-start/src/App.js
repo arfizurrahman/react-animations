24c84ab9ccdf7192aabf70d493d11a67dbecb9d7
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-28 12:59:37 +0600

    Wrapping the transition component

--- animations--01-start/src/App.js
+++ animations--01-start/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -24,6 +25,7 @@ class App extends Component {
             <div className="App">
                 <h1>React Animations</h1>
                 <button
+                    className="Button"
                     onClick={() =>
                         this.setState(prevState => ({
                             showBlock: !prevState.showBlock
@@ -33,25 +35,27 @@ class App extends Component {
                     Toggle
                 </button>
                 <br />
-                {this.state.showBlock ? (
-                    <div
-                        style={{
-                            backgroundColor: "red",
-                            width: 100,
-                            height: 100,
-                            margin: "auto"
-                        }}
-                    ></div>
-                ) : null}
-                {this.state.modalIsOpen ? (
-                    <Modal
-                        show={this.state.modalIsOpen}
-                        closed={this.closeModal}
-                    />
-                ) : null}
-                {this.state.modalIsOpen ? (
-                    <Backdrop show={this.state.modalIsOpen} />
-                ) : null}
+                <Transition
+                    in={this.state.showBlock}
+                    timeout={300}
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    {state => (
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: 100,
+                                height: 100,
+                                margin: "auto",
+                                transition: "opacity 1s ease-out",
+                                opacity: state === "exiting" ? 0 : 1
+                            }}
+                        ></div>
+                    )}
+                </Transition>
+                <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+                {this.state.modalIsOpen ? <Backdrop show /> : null}
                 <button className="Button" onClick={this.showModal}>
                     Open Modal
                 </button>
